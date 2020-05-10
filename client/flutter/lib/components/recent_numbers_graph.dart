1611319fcaf119065aa73b0d80f4e6226659e5bb
Benjamin Swerdlow <swerdlowbenjamin@gmail.com>
2020-05-10 00:10:54 -0700

    new visualization

--- client/flutter/lib/components/recent_numbers_graph.dart
+++ client/flutter/lib/components/recent_numbers_graph.dart
@@ -1,5 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:who_app/constants.dart';
 
 class RecentNumbersGraph extends StatefulWidget {
   const RecentNumbersGraph({
@@ -16,29 +17,45 @@ class RecentNumbersGraph extends StatefulWidget {
 }
 
 class _RecentNumbersGraphState extends State<RecentNumbersGraph> {
+  final startDate = DateTime.utc(2020, 1, 1);
+
   @override
   Widget build(BuildContext context) {
     return LineChart(
       LineChartData(
-        axisTitleData: FlAxisTitleData(show: false),
-        backgroundColor: Color(0xFF008DC9),
-        borderData: FlBorderData(show: false),
-        clipToBorder: true,
-        gridData: FlGridData(show: false),
+        axisTitleData: FlAxisTitleData(
+          show: false,
+        ),
+        borderData: FlBorderData(
+          show: false,
+        ),
+        clipToBorder: false,
+        gridData: FlGridData(
+          getDrawingVerticalLine: (
+            double a,
+          ) {
+            return FlLine(
+              strokeWidth: .25,
+              color: Constants.neutral2Color,
+            );
+          },
+          show: true,
+          drawVerticalLine: true,
+          drawHorizontalLine: true,
+          horizontalInterval: horizontalInterval(),
+          verticalInterval: verticalInterval(),
+        ),
         lineBarsData: [
           LineChartBarData(
-            aboveBarData: BarAreaData(
-              colors: [Color(0xFF008DC9)],
-              show: true,
+            barWidth: 6,
+            colors: [
+              Constants.primaryColor,
+            ],
+            dotData: FlDotData(
+              show: false,
             ),
-            barWidth: 0,
-            belowBarData: BarAreaData(
-              colors: [Color(0xFF6AC7FA)],
-              show: true,
-            ),
-            colors: [Color(0xFF6AC7FA)],
-            dotData: FlDotData(show: false),
             isCurved: false,
+            isStrokeCapRound: true,
             spots: _buildSpots(),
           ),
         ],
@@ -49,9 +66,54 @@ class _RecentNumbersGraphState extends State<RecentNumbersGraph> {
     );
   }
 
+  double verticalInterval() {
+    double total = 0;
+    if (widget.timeseries != null) {
+      for (var snapshot in widget.timeseries) {
+        try {
+          var xAxis = DateTime.fromMillisecondsSinceEpoch(snapshot['epochMsec'],
+                  isUtc: true)
+              .difference(startDate)
+              .inHours
+              .toDouble();
+          if (xAxis > total) {
+            total = xAxis;
+          }
+          // var yAxis = snapshot[widget.timeseriesKey].toDouble();
+          // spots.add(FlSpot(xAxis, yAxis));
+        } catch (e) {
+          print('Error adding point for snapshot: $e');
+        }
+      }
+    } else {
+      final int daysSinceStart =
+          DateTime.now().toUtc().difference(startDate).inDays;
+      total = daysSinceStart.toDouble();
+    }
+    return total / 8;
+  }
+
+  double horizontalInterval() {
+    double total = 0;
+    if (widget.timeseries != null) {
+      for (var snapshot in widget.timeseries) {
+        try {
+          double yAxis = snapshot[widget.timeseriesKey].toDouble();
+          if (yAxis > total) {
+            total = yAxis;
+          }
+        } catch (e) {
+          print('Error adding point for snapshot: $e');
+        }
+      }
+    } else {
+      total = 0;
+    }
+    return total / 4;
+  }
+
   List<FlSpot> _buildSpots() {
     var spots = <FlSpot>[];
-    final startDate = DateTime.utc(2020, 1, 1);
     if (widget.timeseries != null) {
       for (var snapshot in widget.timeseries) {
         try {
